ccd452e5cc8c0dfe708339d75265ca694051ede2
zecollokaris <collo.kariss@gmail.com>
2018-09-10 15:53:14 +0300

    added new goal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <h1> MyGoals </h1>
+    <p>My Goal For Today is {{goal}}</p>
+    `
 })
 export class AppComponent {
-  title = 'Goals';
+  goal = 'Watch Finding Nemo';
 }
