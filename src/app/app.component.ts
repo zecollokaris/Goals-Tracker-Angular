f98d7ed3cc6db80c1462dd1c507668beb1af9c11
zecollokaris <collo.kariss@gmail.com>
2018-09-11 12:01:44 +0300

    created Goals using components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
+//import keyword for importing modules.
 import { Component } from '@angular/core';
+import {Goal} from './goal'
 
+
+//Component annotation is used to add functionality to our code.
+
+//@Component annotation add metadata to our component class.
 @Component({
+  //selector attribute to defines selector for our components.
   selector: 'app-root',
-  template: `
-    <h1> MyGoals </h1>
-    <p>My Goal For Today is {{goal}}</p>
-    `
+  //templateUrl define location for template file.
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
+  //We export the class so that we can import it anywhere we need it in the application.
 export class AppComponent {
-  goal = 'Watch Finding Nemo';
+    goals: string[];
+
+  constructor(){
+    this.goals = ["Watch Finding Nemo", "Buy Cookies","Get new phonecase"];
+  }
 }
