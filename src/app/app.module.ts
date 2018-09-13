8b787ad0d8c271d58683e64be8431b95cc6c3f38
zecollokaris <collo.kariss@gmail.com>
2018-09-13 11:36:41 +0300

    added line strick through

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { GoalComponent } from './goal/goal.component';
 import { GoalDetailsComponent } from './goal-details/goal-details.component';
+import { StrikethroughDirective } from './strikethrough.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     GoalComponent,
-    GoalDetailsComponent
+    GoalDetailsComponent,
+    StrikethroughDirective
   ],
   imports: [
     BrowserModule
