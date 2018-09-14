b181e483200990f57fe22e4a39be6ec49565c669
zecollokaris <collo.kariss@gmail.com>
2018-09-14 12:26:14 +0300

    added submit method

--- src/app/goal-form/goal-form.component.ts
+++ src/app/goal-form/goal-form.component.ts
@@ -1,14 +1,20 @@
 import {Goal} from '../goal';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output,EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-goal-form',
   templateUrl: './goal-form.component.html',
   styleUrls: ['./goal-form.component.css']
 })
+
 export class GoalFormComponent implements OnInit {
 
     newGoal=new Goal(0,"","",new Date());
+    @Output() addGoal=new EventEmitter<Goal>();
+    
+    submitGoal(){
+        this.addGoal.emit(this.newGoal);
+    }
 
   constructor() { }
 
