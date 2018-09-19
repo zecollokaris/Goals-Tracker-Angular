544df6f045601bdef7581afdc2624b3cddfa4c94
zecollokaris <collo.kariss@gmail.com>
2018-09-19 12:34:15 +0300

    changed and changed display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,11 +17,9 @@ import {Goal} from './goal'
   //We export the class so that we can import it anywhere we need it in the application.
 export class AppComponent {
     goals = [
-      new Goal(1, 'Watch Finding Nemo', 'In the house', new Date(2018, 3, 14)),
-      new Goal(2, 'Buy Cookies', 'Cause im hungry', new Date(2018, 3, 14)),
-      new Goal(3, 'Get new Phone Case', 'to keep my phone safe', new Date(2018, 3, 14)),
-      new Goal(4, 'Get Dog Food', 'Cause it must be hungry', new Date(2018, 3, 14)),
-      new Goal(5, 'Solve math homework', 'oohhh noooo!', new Date(2018, 3, 14)),
-      new Goal(6, 'Plot my world domination plan', 'hhuuuh?', new Date(2018, 3, 14)),
+      new Goal(1, 'Want to add Some Goals', '.', new Date(2018, 3, 14)),
+      new Goal(2, 'Add Goals and Keep track of them', '.', new Date(2018, 3, 14)),
+      new Goal(3, 'Welcome!!>>', '.', new Date(2018, 3, 14)),
+
     ]
 }
